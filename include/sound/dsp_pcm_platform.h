793a0917be4f0d6507cd16871c927fd0b32d87a7
Jibin George <jibin.george@ignitarium.com>
2026-05-16 17:36:46 +0530

    ALSA: move rpsmg objects into a separate header file
    - Move rpsmg objects into a separate header file
    - Use version number for header file
    - Correct spelling mistake for macro FRAME_PERIOD

---
+++ include/sound/dsp_pcm_platform.h
@@ -0,0 +1,69 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef __DSP_PCM_PLATFORM_H
+#define __DSP_PCM_PLATFORM_H
+
+#define DSP_CTRL_RPMSG_VERSION (0x0U)
+#define BUFFER_LEN (4096U)          /* 4kB */
+#define FRAME_PERIOD (4U)
+
+/* DSP Control Message type */
+#define CONFIG_REQ       0x2001     /* CA-DSP */
+#define PCM_START        0x2002     /* CA-DSP */
+#define PCM_PAUSE        0x2003     /* CA-DSP */
+#define PCM_RESUME       0x2004     /* CA-DSP */
+#define PCM_STOP         0x2005     /* CA-DSP */
+#define POS_QUERY        0x2006     /* CA-DSP */
+#define PCM_STATUS       0x8000     /* DSP-CA (reply/heartbeat/event) */
+#define CONFIG_REPLY     0xA001     /* DSP-CA (config reply) */
+#define POS_REPLY        0xA006     /* DSP-CA (position/cursor reply) */
+
+struct rpmsg_hdr {
+	uint16_t version;
+	uint16_t msg_type;
+	uint32_t msg_id;
+	uint32_t stream_id;
+	uint32_t payload_len;
+} __attribute__((packed));
+
+struct trigger_req {
+	uint32_t cmd;               /* START/STOP/PAUSE/RESUME/DRAIN */
+} __attribute__((packed));
+
+struct dsp_config_req_msg {
+	uint32_t dir;               /* playback=0, capture=1 */
+	uint32_t in_rate;           /* app rate for PB/ Hw rate for REC */
+	uint32_t in_channels;
+	uint32_t in_format;         /* ALSA format */
+	uint32_t out_rate;          /* Hw rate for PB / app rate for REC */
+	uint32_t out_channels;
+	uint32_t out_format;
+	uint64_t pcm_rb_phys;       /* DSP write/read target for DSP */
+	uint32_t pcm_rb_size;
+	uint64_t hw_rb_phys;
+	uint32_t hw_rb_size;
+	uint32_t period_frames;     /* matches ALSA period */
+	uint32_t periods;
+} __attribute__((packed));
+
+struct dsp_status_msg {
+	uint32_t dir;               /* playback=0, capture=1 */
+	uint32_t status;            /* RUNNING, STOPPED, PAUSED, etc. */
+	uint64_t pcm_dsp_pos;       /* frames consumed since stream start */
+	uint64_t hw_rb_write_pos;   /* optional: frames written to LP HW RB */
+	uint32_t period_frames;     /* echo for safety */
+	uint32_t rb_frames;         /* total frames in PCM RB */
+} __attribute__((packed));
+
+union rpmsg_payload {
+	struct trigger_req trigger;
+	struct dsp_config_req_msg dsp_config;
+	struct dsp_status_msg dsp_status;
+};
+
+struct rpmsg_packet {
+	struct rpmsg_hdr header;
+	union rpmsg_payload payload;
+} __attribute__((packed));
+
+#endif /* __DSP_PCM_PLATFORM_H */
--- sound/soc/dsp_pcm_platform.c
+++ sound/soc/dsp_pcm_platform.c
@@ -24,89 +24,10 @@
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
 
-#define BUFFER_LEN (32768U)         /* 32kB */
+#include <sound/dsp_pcm_platform.h>
+
 #define MAX_DEVICES (4U)            /* Maximum number of ALSA instances */
 #define DSP_RESP_TIMEOUT_MS (100U)
-
-/* DSP Control Message type */
-#define CONFIG_REQ       0x2001     /* CA-DSP */
-#define PCM_START        0x2002     /* CA-DSP */
-#define PCM_PAUSE        0x2003     /* CA-DSP */
-#define PCM_RESUME       0x2004     /* CA-DSP */
-#define PCM_STOP         0x2005     /* CA-DSP */
-#define POS_QUERY        0x2006     /* CA-DSP */
-#define PCM_STATUS       0x8000     /* DSP-CA (reply/heartbeat/event) */
-#define CONFIG_REPLY     0xA001     /* DSP-CA (config reply) */
-#define POS_REPLY        0xA006     /* DSP-CA (position/cursor reply) */
-
-struct rpmsg_hdr {
-	uint16_t version;
-	uint16_t msg_type;
-	uint32_t msg_id;
-	uint32_t stream_id;
-	uint32_t payload_len;
-} __attribute__((packed));
-
-struct open_req_messsage {
-	uint32_t dir;               /* playback=0, capture=1 */
-	uint32_t rate;              /* app rate */
-	uint32_t channels;          /* app channels */
-	uint32_t format;            /* ALSA snd_pcm_format_t */
-	uint32_t period_frames;     /* ALSA period size in frames */
-	uint32_t periods;           /* period count */
-	uint64_t pcm_rb_phys;       /* phys addr of PCM RB */
-	uint32_t pcm_rb_size;
-} __attribute__((packed));
-
-struct open_resp_message {
-	int32_t status;
-	int32_t stream_id;
-	uint32_t hw_rate;
-	uint32_t hw_channels;
-	uint32_t hw_format;
-	uint64_t hw_rb_phys;
-	uint32_t hw_rb_size;
-} __attribute__((packed));
-
-struct trigger_req {
-	uint32_t cmd;               /* START/STOP/PAUSE/RESUME/DRAIN */
-} __attribute__((packed));
-
-struct dsp_config_req_msg {
-	uint32_t dir;               /* playback=0, capture=1 */
-	uint32_t in_rate;           /* app rate for PB/ Hw rate for REC */
-	uint32_t in_channels;
-	uint32_t in_format;         /* ALSA format */
-	uint32_t out_rate;          /* Hw rate for PB / app rate for REC */
-	uint32_t out_channels;
-	uint32_t out_format;
-	uint64_t pcm_rb_phys;       /* DSP write/read target for DSP */
-	uint32_t pcm_rb_size;
-	uint64_t hw_rb_phys;
-	uint32_t hw_rb_size;
-	uint32_t period_frames;     /* matches ALSA period */
-	uint32_t periods;
-} __attribute__((packed));
-
-struct dsp_status_msg {
-	uint32_t dir;               /* playback=0, capture=1 */
-	uint32_t status;            /* RUNNING, STOPPED, PAUSED, etc. */
-	uint64_t pcm_dsp_pos;       /* frames consumed since stream start */
-	uint64_t hw_rb_write_pos;   /* optional: frames written to LP HW RB */
-	uint32_t period_frames;     /* echo for safety */
-	uint32_t rb_frames;         /* total frames in PCM RB */
-} __attribute__((packed));
-
-union rpmsg_payload {
-	struct trigger_req trigger;
-	struct dsp_config_req_msg dsp_config;
-	struct dsp_status_msg dsp_status;
-};
-
-struct rpmsg_packet {
-	struct rpmsg_hdr header;
-	union rpmsg_payload payload;
-} __attribute__((packed));
 
 /* Per-stream state */
 struct rcar_alsa_stream {
@@ -235,8 +156,8 @@ static int rcar_alsa_fe_pcm_open(struct snd_pcm_substream *sub)
 	runtime->hw.buffer_bytes_max = BUFFER_LEN;
 	runtime->hw.period_bytes_min = 1024;
 	runtime->hw.period_bytes_max = 1024;
-	runtime->hw.periods_min = 32;
-	runtime->hw.periods_max = 32;
+	runtime->hw.periods_min = FRAME_PERIOD;
+	runtime->hw.periods_max = FRAME_PERIOD;
 
 	runtime->private_data = d;
 
@@ -289,7 +210,7 @@ static int rcar_audio_fe_pcm_hw_params(struct snd_pcm_substream *sub,
 	mutex_unlock(&dbg->lock);
 
 	/* header */
-	msg.header.version = 1;
+	msg.header.version = DSP_CTRL_RPMSG_VERSION;
 	msg.header.msg_type = CONFIG_REQ;
 	msg.header.msg_id = 1;
 	msg.header.stream_id =
@@ -312,7 +233,7 @@ static int rcar_audio_fe_pcm_hw_params(struct snd_pcm_substream *sub,
 		(uint64_t)dma_to_phys(&pdev->dev, s->hw_dma_handle);
 	msg.payload.dsp_config.hw_rb_size = (uint64_t)s->hw_buf_size;
 	msg.payload.dsp_config.period_frames = 256;
-	msg.payload.dsp_config.periods = 32;
+	msg.payload.dsp_config.periods = FRAME_PERIOD;
 
 	rpmsg_send_dsp(msg, d);
 
@@ -387,7 +308,7 @@ static int rcar_audio_fe_pcm_trigger(struct snd_pcm_substream *sub, int cmd)
 		pr_info("rcar_audio_fe: %s START\n", __func__);
 
 		/* Notify DSP */
-		msg.header.version = 1;
+		msg.header.version = DSP_CTRL_RPMSG_VERSION;
 		msg.header.msg_type = PCM_START;
 		msg.header.msg_id = 1;
 		msg.header.stream_id =
@@ -402,7 +323,7 @@ static int rcar_audio_fe_pcm_trigger(struct snd_pcm_substream *sub, int cmd)
 		pr_info("rcar_audio_fe: %s RESUME\n", __func__);
 
 		/* Notify DSP */
-		msg.header.version = 1;
+		msg.header.version = DSP_CTRL_RPMSG_VERSION;
 		msg.header.msg_type = PCM_RESUME;
 		msg.header.msg_id = 1;
 		msg.header.stream_id =
@@ -417,7 +338,7 @@ static int rcar_audio_fe_pcm_trigger(struct snd_pcm_substream *sub, int cmd)
 		pr_info("rcar_audio_fe: %s STOP\n", __func__);
 
 		/* Notify DSP */
-		msg.header.version = 1;
+		msg.header.version = DSP_CTRL_RPMSG_VERSION;
 		msg.header.msg_type = PCM_STOP;
 		msg.header.msg_id = 1;
 		msg.header.stream_id =
@@ -431,7 +352,7 @@ static int rcar_audio_fe_pcm_trigger(struct snd_pcm_substream *sub, int cmd)
 		pr_info("rcar_audio_fe: %s SUSPEND\n", __func__);
 
 		/* Notify DSP */
-		msg.header.version = 1;
+		msg.header.version = DSP_CTRL_RPMSG_VERSION;
 		msg.header.msg_type = PCM_PAUSE;
 		msg.header.msg_id = 1;
 		msg.header.stream_id =
