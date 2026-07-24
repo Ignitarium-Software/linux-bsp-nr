bf5955b9ef3d29c8f3ecc14ad9e22951c6cac219
Sourav Raj K K <sourav.raj@ignitarium.com>
2026-07-24 13:48:53 +0530

    sound: soc: bugfix
     * Issue: even after playback is stopped, noise
       is heard.
    
     * this issue is because ALSA FE sends stop command to CR
       before DSP. Hence DSP copies to the buffer even after CR
       clear it. So DSP is notified first, followed by CR core.
    
    Signed-off-by: Sourav Raj K K <sourav.raj@ignitarium.com>

--- sound/soc/rcar_alsa_fe.c
+++ sound/soc/rcar_alsa_fe.c
@@ -479,16 +479,6 @@ static int rcar_audio_fe_pcm_trigger(struct snd_pcm_substream *sub, int cmd)
 	case SNDRV_PCM_TRIGGER_STOP:
 		pr_info("rcar_audio_fe: %s STOP\n", __func__);
 
-        /* notify CR */
-        msg.header.version = CR_CTRL_RPMSG_VERSION;
-        msg.header.msg_type = TRIGGER_STOP;
-        msg.header.msg_id = 1;
-        msg.header.stream_id =
-            (sub->stream == SNDRV_PCM_STREAM_PLAYBACK) ? 0: 1;
-        msg.header.payload_len = sizeof(struct open_req_msg);
-
-        rpmsg_send_cr(msg, d);
-
 		/* Notify DSP */
 		msg.header.version = DSP_CTRL_RPMSG_VERSION;
 		msg.header.msg_type = PCM_STOP;
@@ -499,6 +489,16 @@ static int rcar_audio_fe_pcm_trigger(struct snd_pcm_substream *sub, int cmd)
 		msg.payload.trigger.cmd = PCM_STOP;
 
 		rpmsg_send_dsp(msg, d);
+
+        /* notify CR */
+        msg.header.version = CR_CTRL_RPMSG_VERSION;
+        msg.header.msg_type = TRIGGER_STOP;
+        msg.header.msg_id = 1;
+        msg.header.stream_id =
+            (sub->stream == SNDRV_PCM_STREAM_PLAYBACK) ? 0: 1;
+        msg.header.payload_len = sizeof(struct open_req_msg);
+
+        rpmsg_send_cr(msg, d);
 		return 0;
 
 	case SNDRV_PCM_TRIGGER_SUSPEND:
